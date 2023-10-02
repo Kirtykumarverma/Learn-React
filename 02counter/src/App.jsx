0b7614fcefe81751dfb3859fe00b15fc4493fd54
Kirty Kumar Verma <kirty.verma1996@gmail.com>
2023-10-02 14:26:27 +0530

    counter projec completed

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -7,16 +7,28 @@ function App() {
   const [counter, setCounter] = useState(0);
 
   const addValue = () => {
-    counter = counter + 1;
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
   };
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <>
       <h1>Chai aur react</h1>
       <h2>Counter value : {counter}</h2>
 
       <button onClick={addValue}>Add Value {counter}</button>
+
       <br />
-      <button>Remove Value {counter}</button>
+      <br />
+
+      <button onClick={removeValue}>Remove Value {counter}</button>
     </>
   );
 }
